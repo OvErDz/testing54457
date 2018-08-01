e9ca56d0a10329b596b806917b6b9d9559b23a9c
OvErDz <41547554+OvErDz@users.noreply.github.com>
2018-08-01 14:11:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 var Canvas = require('canvas');// npm i canvas
 var jimp = require('jimp');// npm i jimp 
-const fs = require("fs");// npm i fs
+const fs = require("fs");
 
       bot.on('guildMemberAdd', member => {
       const welcomer =  member.guild.channels.find('name', 'welcome');
