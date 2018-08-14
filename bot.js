34e0a953c240c233c74f6c0e74d7668a15a2d79c
OvErDz <41547554+OvErDz@users.noreply.github.com>
2018-08-14 11:45:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame('=help|=inv','https://www.twitch.tv/OvErDz1');
+  client.user.setGame('=help|=inv ','https://www.twitch.tv/OvErDz1');
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
